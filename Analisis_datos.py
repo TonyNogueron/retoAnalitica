d52985b3be5c63995d806dfa9332b7d088e9246b
ant20ni02 <ant20ni02@XD-Computer.local>
2022-05-10 12:41:00 -0500

    Not clean

--- Analisis_datos.py
+++ Analisis_datos.py
@@ -1,3 +1,10 @@
 import pandas as pd
+import numpy as np
+
+
 tabla = pd.read_csv("./data.csv", low_memory=False)
 carTable = tabla[["manufacturer","transmission_type", "fuel_type", "co_emissions"]]
+carTable.replace("", np.nan, inplace=True)
+carTable = carTable.dropna(inplace=True)
+#print(carTable)
+
