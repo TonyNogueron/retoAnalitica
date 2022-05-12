1136dd96aa0c482d0b564c27c5f6d2f3f0f46793
Herr Turner <georgeeturnere@gmail.com>
2022-05-11 22:07:24 -0500

    Added hitogram, box and whiskers and heatmap graphs

--- Analisis_datos.py
+++ Analisis_datos.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
 
 tabla =  pd.read_csv("data.csv", low_memory=False) #Created data frame from our dataset
 tabla = tabla[["year","manufacturer","transmission_type", "fuel_type", "co_emissions", "co2","engine_capacity","noise_level"]] #Filters dataframe by given columns
@@ -13,3 +16,34 @@ for column in tabla:
         print("Rango de "+str(column)+":" , tabla[column].max()-tabla[column].min())
     except:
         print("Rango de "+str(column)+": No existe rango para este tipo de dato")
+
+fig, ax = plt.subplots(figsize=(6,4))
+
+tabla_CO2 = tabla["co2"]
+tabla_CO = tabla["co_emissions"]
+
+ax.hist(tabla_CO2,ec='black')
+
+plt.xlabel('Emisiones de CO2')
+plt.ylabel('Frecuencia')
+plt.show()
+
+fig, ax = plt.subplots(figsize=(6,4), sharey=True )
+
+sns.boxplot(x = tabla["co_emissions"])
+
+plt.show()
+
+#Creamos un heatmap con otros datos porque creemos que no se ajusta a nuestra base de datos, no son datos reales
+
+datos = {'ciudades': np.tile (['Londres', 'Glasgow', 'Westminster', 'Birmingham', 'Cambridge'], 5),
+        'anio': np.repeat ([2000, 2001, 2002, 2003, 2004], 5),
+        'ventas': np.random.randint (100, 500, size = 25)
+        }
+
+df = pd.DataFrame(datos, columns = ['ciudades', 'anio', 'ventas'])
+df = df.pivot ('ciudades', 'anio', 'ventas')
+
+sns.heatmap(df, linewidths = .5, annot = True, fmt="d")
+
+plt.show()
